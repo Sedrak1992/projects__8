24fb11727be1f11fed3f20e70bca92213d81fcf4
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-02 17:31:29 +0400

    a

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let gallery = document.getElementById("yay-no-balloons");
 gallery.style.display = "none";
 
 const handleClick = (event) => {
-
-
-
   if (event.target.className === "item") {
     event.target.style.backgroundColor = "#ededed";
     event.target.textContent = "POP!";
@@ -25,30 +22,5 @@ items.forEach((items) => {
   items.addEventListener("mouseenter", handleClick);
 });
 
-const randomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
-
-// document.addEventListener('mouseover', function(e) {
-
-//             if (e.target.className === "item") {
-
-//                 e.target.style.backgroundColor = "#ededed";
-//                 e.target.textContent = "POP!";
-//                 popped++;
-//                 removeEvent(e);
-//         checkAllPopped();
-//     }
-// });
-
-// function removeEvent(e) {
-//     e.target.removeEventListener('mouseover', function() {
-
-//     })
-// };
-
-// function checkAllPopped() {
-//     if (popped === 24) {
-// let gallery = document.querySelector('#Container');
-// gallery.innerHTML = '';
-// message.style.display = 'block';
-//     }
-// };
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
